5e83afeeff58d660865eeae4664a20469de66bef
Julian Moreno <julian.m.personal@gmail.com>
2023-10-12 00:24:05 -0300

    Se sube ultima actualizacion.

--- src/main/java/com/example/proxy/WeatherApplication.java
+++ src/main/java/com/example/proxy/WeatherApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class ProxyApplication {
+public class WeatherApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProxyApplication.class, args);
+		SpringApplication.run(WeatherApplication.class, args);
 	}
 
 }
