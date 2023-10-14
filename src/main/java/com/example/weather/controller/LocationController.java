7bf2437413ee6545d9030be972e04be57e29a767
Julian Moreno <julian.m.personal@gmail.com>
2023-10-14 01:35:46 -0300

    Se sube ultima actualizacion.

--- src/main/java/com/example/weather/controller/LocationController.java
+++ src/main/java/com/example/weather/controller/LocationController.java
@@ -3,6 +3,7 @@ package com.example.weather.controller;
 import com.example.weather.dto.response.LocationResDTO;
 import com.example.weather.exception.TechnicalException;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -17,7 +18,7 @@ import java.util.List;
 @Tag(name = "/location", description = "Servicio para obtener informacion detallada de una ubicación")
 public interface LocationController {
 
-    @Operation(summary  = "Obtiene la lista de ubicaciones por nombre.")
+    @Operation(summary  = "Obtiene la lista de ubicaciones por localizedName.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
                           description = "success",
@@ -25,7 +26,8 @@ public interface LocationController {
             @ApiResponse(responseCode = "500",
                           description = "Internal Server Error",
                              content  =  @Content)})
-    ResponseEntity<List<LocationResDTO>> getLocationByLocalizedName(@RequestParam("localizedName")String localizedName) throws TechnicalException;
+    ResponseEntity<List<LocationResDTO>> getLocationByLocalizedName(@Parameter(description = "Esta propiedad se utiliza para especificar el nombre de la ciudad o área para la cual deseas obtener información meteorológica", required = true)
+                                                                    @RequestParam("localizedName")String localizedName) throws TechnicalException;
 
 
     @Operation(summary = "Obtiene la ubicación por clave.")
@@ -36,5 +38,6 @@ public interface LocationController {
             @ApiResponse(responseCode = "500",
                           description = "Internal Server Error",
                              content  =  @Content)})
-    ResponseEntity<LocationResDTO> getLocationByKey(@PathVariable("key")String key) throws TechnicalException;
+    ResponseEntity<LocationResDTO> getLocationByKey(@Parameter(description = "Clave única que identifica de manera única una ubicación geográfica específica", required = true)
+                                                    @PathVariable("key")String key) throws TechnicalException;
 }
--- src/main/java/com/example/weather/dto/request/WeatherRqDTO.java
+++ src/main/java/com/example/weather/dto/request/WeatherRqDTO.java
@@ -1,5 +1,6 @@
 package com.example.weather.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class WeatherRqDTO {
+    @Schema(description = "Clave única que identifica la ubicación para obtener información meteorológica", required = true)
     String key;
 }
